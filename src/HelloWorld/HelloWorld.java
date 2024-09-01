e48122b377369c73c38dc8f130a9666a349bd7c1
dit <89990545+dittttt@users.noreply.github.com>
2024-09-01 15:30:30 +0800

    Add personalized greeting feature

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,7 +1,32 @@
 package HelloWorld;
 
+import java.util.Scanner;
+
 public class HelloWorld {
-	public static void main(String[] args) {
-        System.out.println("Hello World!"); 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+
+        String capitalizedName = capitalizeFirstLetters(name);
+
+        System.out.println("Hello, " + capitalizedName + "!");
+
+        scanner.close();
+    }
+
+    public static String capitalizeFirstLetters(String str) {
+        String[] words = str.split(" ");
+        StringBuilder capitalized = new StringBuilder();
+
+        for (String word : words) {
+            if (word.length() > 0) {
+                capitalized.append(Character.toUpperCase(word.charAt(0)))
+                           .append(word.substring(1).toLowerCase()).append(" ");
+            }
+        }
+
+        return capitalized.toString().trim();
     }
 }
